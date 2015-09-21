c0ea71641a8afd8fd671e3beb404bd52869b0626
chan xuehong <chanxuehong@gmail.com>
2015-09-21 08:50:38 +0800

    no message

--- model/bind_wechat.go
+++ model/bind_wechat.go
@@ -1,1 +1,66 @@
 package model
+
+import (
+	"fmt"
+
+	"github.com/aiyi/go-user/db"
+)
+
+type BindWechatParams struct {
+	UserId   int64  `sqlx:"user_id"` // 绑定到这个用户
+	OpenId   string `sqlx:"openid"`
+	Nickname string `sqlx:"nickname"`
+}
+
+// 给用户绑定微信.
+//  调用该函数前要确认该用户可以绑定, 该微信未被绑定
+func BindWechat(para *BindWechatParams) (err error) {
+	parax := struct {
+		*BindWechatParams
+		AuthType int64 `sqlx:"auth_type"`
+	}{
+		BindWechatParams: para,
+		AuthType:         AuthTypeWechat,
+	}
+
+	tx, err := db.GetDB().Beginx()
+	if err != nil {
+		return
+	}
+
+	// user_wechat 表增加一个 item
+	stmt1, err := tx.Prepare("insert into user_wechat(user_id, nickname, openid, has_bound) values(?, ?, ?, 1)")
+	if err != nil {
+		tx.Rollback()
+		return
+	}
+	if _, err = stmt1.Exec(parax.UserId, parax.Nickname, parax.OpenId); err != nil {
+		tx.Rollback()
+		return
+	}
+
+	// user 更新 item
+	stmt2, err := tx.PrepareNamed("update user set auth_types = auth_types|:auth_type where id=:user_id and has_fixed=1 and auth_types&:auth_type=0")
+	if err != nil {
+		tx.Rollback()
+		return
+	}
+	rslt, err := stmt2.Exec(parax)
+	if err != nil {
+		tx.Rollback()
+		return
+	}
+	rowsAffected, err := rslt.RowsAffected()
+	if err != nil {
+		tx.Rollback()
+		return
+	}
+	if rowsAffected != 1 {
+		err = fmt.Errorf("绑定微信 %s 到用户 %d 失败", para.OpenId, para.UserId)
+		tx.Rollback()
+		return
+	}
+
+	err = tx.Commit()
+	return
+}
