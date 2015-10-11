3b7ec8c054afa7a0137476621e05e6c764ad00f3
chan xuehong <chanxuehong@gmail.com>
2015-10-11 14:22:32 +0800

    no message

--- mc/mc.config.go
+++ mc/mc.config.go
@@ -1,9 +1,15 @@
 package mc
 
 import (
+	"errors"
+
 	"github.com/aiyi/go-user/config"
 )
 
 func getServerList() ([]string, error) {
-	return config.ConfigData.MemcacheServerList, nil
+	servers := config.ConfigData.MemcacheServerList
+	if len(servers) == 0 {
+		return nil, errors.New("empty memcache server list")
+	}
+	return servers, nil
 }
--- securitykey/securitykey.config.go
+++ securitykey/securitykey.config.go
@@ -1,10 +1,16 @@
 package securitykey
 
 import (
+	"errors"
+
 	"github.com/aiyi/go-user/config"
 )
 
 // 从私密存储上获取安全key
 func getKey() ([]byte, error) {
-	return config.ConfigData.SecurityKey, nil
+	key := config.ConfigData.SecurityKey
+	if len(key) != 128 {
+		return nil, errors.New("incorrect security key")
+	}
+	return key, nil
 }
