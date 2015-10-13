caf4af68e8166b5234889e3324f862bb599b3005
chan xuehong <chanxuehong@gmail.com>
2015-10-13 21:38:19 +0800

    no message

--- frontend/token/handler/refresh.go
+++ frontend/token/handler/refresh.go
@@ -3,25 +3,40 @@ package handler
 import (
 	"time"
 
+	"github.com/chanxuehong/util/security"
 	"github.com/gin-gonic/gin"
 	"github.com/golang/glog"
 
 	"github.com/aiyi/go-user/frontend/errors"
 	"github.com/aiyi/go-user/frontend/session"
 	"github.com/aiyi/go-user/frontend/token"
+	"github.com/aiyi/go-user/model"
 )
 
 // 刷新 token
 func RefreshHandler(ctx *gin.Context) {
-	// MustAuthHandler(ctx)
-	tk := ctx.MustGet("token").(*token.Token)
-	if tk.AuthType == token.AuthTypeGuest { // 如果是 guest 用户直接返回
+	tkString := ctx.Request.Header.Get("x-token")
+	if tkString == "" {
+		ctx.JSON(200, errors.ErrTokenMissing)
+		return
+	}
+
+	// 解析 token
+	tk := &token.Token{}
+	if err := tk.Decode([]byte(tkString)); err != nil {
+		glog.Errorln(err)
+		ctx.JSON(200, errors.ErrTokenDecodeFailed)
+		return
+	}
+
+	// 如果是 guest 直接返回
+	if tk.AuthType == token.AuthTypeGuest {
 		resp := struct {
 			*errors.Error
 			Token string `json:"token"`
 		}{
 			Error: errors.ErrOK,
-			Token: ctx.MustGet("token_string").(string),
+			Token: tkString,
 		}
 		ctx.JSON(200, &resp)
 		return
@@ -38,14 +53,60 @@ func RefreshHandler(ctx *gin.Context) {
 			Token string `json:"token"`
 		}{
 			Error: errors.ErrOK,
-			Token: ctx.MustGet("token_string").(string),
+			Token: tkString,
 		}
 		ctx.JSON(200, &resp)
 		return
 	}
-	if tk.ExpirationAccess >= tk.ExpirationRefresh { // 暴力的结束, 防止客户端循环的刷新
+	if tk.ExpirationAccess >= tk.ExpirationRefresh { // 提前 1200秒 暴力的结束, 防止客户端循环的刷新
 		ctx.JSON(200, errors.ErrTokenRefreshExpired)
 		return
+	}
+
+	// 现在我们真的需要刷新token啦!!!
+
+	// 获取 Session 并判断与 token 是否匹配
+	ss, err := session.Get(tk.SessionId)
+	if err != nil {
+		glog.Errorln(err)
+		if err == errors.ErrNotFound {
+			ctx.JSON(200, errors.ErrTokenInvalid)
+			return
+		}
+		ctx.JSON(200, errors.ErrInternalServerError)
+		return
+	}
+	if !security.SecureCompareString(tk.Signatrue, ss.TokenSignature) {
+		ctx.JSON(200, errors.ErrTokenInvalid)
+		return
+	}
+
+	// 获取用户信息并判断与 token, session 是否一致
+	user, err := model.Get(ss.UserId)
+	if err != nil {
+		glog.Errorln(err)
+		if err == model.ErrNotFound {
+			ctx.JSON(200, errors.ErrTokenInvalid)
+			return
+		}
+		ctx.JSON(200, errors.ErrInternalServerError)
+		return
+	}
+	bindType, err := token.GetBindType(tk.AuthType)
+	if err != nil {
+		glog.Errorln(err)
+		ctx.JSON(200, errors.ErrTokenInvalid)
+		return
+	}
+	if user.BindTypes&bindType == 0 {
+		ctx.JSON(200, errors.ErrTokenInvalid)
+		return
+	}
+	if tk.AuthType == token.AuthTypeEmailPassword || tk.AuthType == token.AuthTypePhonePassword {
+		if ss.PasswordTag != user.PasswordTag {
+			ctx.JSON(200, errors.ErrTokenInvalid)
+			return
+		}
 	}
 
 	tk2 := token.Token{
@@ -65,7 +126,6 @@ func RefreshHandler(ctx *gin.Context) {
 		return
 	}
 
-	ss := ctx.MustGet("session").(*session.Session)
 	ss.TokenSignature = tk2.Signatrue
 	if err = session.Set(tk2.SessionId, ss); err != nil {
 		glog.Errorln(err)
